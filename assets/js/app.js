0b8780ebe18e73d3afdcdad4b135cf7d2dd3e906
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-04-17 16:14:44 +0200

    push per i tutor

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,19 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            dischi: [],
         }
     },
     methods: {
 
     },
+    mounted() {
+        axios.get('assets/json/dischi.json')
+            .then(response => {
+                this.dischi = response.data
+            })
+            .catch(error => {
+                console.error('controlla il percorso');
+            });
+    }
 }).mount('#app')
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+//assolutamente intuile, volevo testare se fossi in grado di prenderli
+/* $jsonDischi = file_get_contents('./assets/json/dischi.json');
+$arrayDischi = json_decode($jsonDischi, true);
+var_dump($arrayDischi); */
+?>
+
 <!doctype html>
 <html lang='en'>
 
@@ -9,13 +16,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
 
-<body>
+<body class="bg-dark">
     <div id='app'>
-        <h1>Hello World</h1>
+        <div class="container">
+            <div class="row ">
+                <div class="col-4">
+                    <div class="card g-2" v-for="(disco, index) in dischi" :key="index">
+                        <img :src="disco.poster" alt="">
+                        <div class="text-center">
+                            <h5>{{ disco.title }}</h5>
+                            <h5>{{ disco.author }}</h5>
+                            <h5>{{ disco.year }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Development only cdn, disable in production -->
     <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+
+    <!-- Axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
@@ -26,4 +49,3 @@
 </body>
 
 </html>
-
